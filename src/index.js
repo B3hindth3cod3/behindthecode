de1b78cbef06d4c152d40729249326cfc1ccec5e
kapotaalan <kapotaalan@gmail.com>
2021-07-25 22:08:21 +0200

    today

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import logo from './heroimage.jpg';
+import {FaGithub, FaEnvelope, FaInstagram} from "react-icons/fa";
 
 function Webpage () {
   return <div>
@@ -27,19 +28,22 @@ function Webpage () {
   <div class="about">
     <h1>About Me</h1>
     <h2>ENGINEER | DEVELOPER</h2>
-    <p class="lead text-muted">Using Engineering and Programming to answer design problems with honest solutions.</p>
-  </div>
-  <div>
-    Social Media
+    <p class="lead text-muted">Using Engineering and Programming to answer design problems with honest 
+    solutions. Join me on my journey in becoming a Full Stack Developer. </p>
   </div>
 
-  <div>
-    Portfolio
+ {/*Social Media*/}
+ <div class="media">
+   <div class="socialinsta">
+     <a href="https://www.instagram.com/behind.the_code/"><FaInstagram /> </a> 
+   </div>
+   <div class="socialgit">
+     <a href="https://github.com/kapotaalan"><FaGithub /> </a>
+   </div>
+   <div class="socialemail">
+     <FaEnvelope/>
+   </div>
   </div>
-  <div>
-    Projects
-  </div>
-
 </div>
 }
 
